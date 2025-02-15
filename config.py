23e90cce76a18958b333ab05aa65c451497dfb83
hackmir <dudolabss@yandex.ru>
2025-02-15 22:30:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "7822564006:AAEupixoT3cMQ0YIfsU9JFX7aqrdyBvY6CA"  # Замените на ваш токен
-ADMIN_ID = 7429471411  # Замените на ваш ID в Telegram
+TOKEN = ""  # Замените на ваш токен
+ADMIN_ID =  # Замените на ваш ID в Telegram
